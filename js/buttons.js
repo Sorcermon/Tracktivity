2f8cebeb2f1ff0ec9acef8bc06ce248ee768c6a5
Spencer <spencer.monro9@gmail.com>
2016-06-05 23:40:47 +1000

    Updated interface and Firebase initialisation

--- js/buttons.js
+++ js/buttons.js
@@ -1,23 +1,23 @@
-// // Initialize Firebase
-// var config = {
-// 	apiKey: "AIzaSyCJjuWMPGkzOInnl0ZZXrjMHRLLOObMMaA",
-// 	authDomain: "project-3239275781682935411.firebaseapp.com",
-// 	databaseURL: "https://project-3239275781682935411.firebaseio.com",
-// 	storageBucket: "",
-// };
-// firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCJjuWMPGkzOInnl0ZZXrjMHRLLOObMMaA",
+    authDomain: "project-3239275781682935411.firebaseapp.com",
+    databaseURL: "https://project-3239275781682935411.firebaseio.com",
+    storageBucket: "project-3239275781682935411.appspot.com",
+};
+firebase.initializeApp(config);
 
 var numberHaircuts = 0;
 
 function setup() {
-	document.getElementById('haircutNo').innerHTML = numberHaircuts;
+    document.getElementById('haircutNo').innerHTML = numberHaircuts;
 }
 
 function updateHaircuts(n) {
-	numberHaircuts+= n;
-	document.getElementById('haircutNo').innerHTML = numberHaircuts;
+    numberHaircuts+= n;
+    document.getElementById('haircutNo').innerHTML = numberHaircuts;
 }
 
 function updateHaircuts2(n) {
-	firebase.database(),ref('')
+    firebase.database(),ref('')
 }
