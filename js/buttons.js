246004781b835a26dc1ce035afd3578c36bf0fc6
Spencer Monro <spencer.monro9@gmail.com>
2016-07-31 17:22:30 +1000

    Professionalised the interface
    Made the three categories all have proper names and made buttons work
    (locally) for each one

--- js/buttons.js
+++ js/buttons.js
@@ -8,14 +8,28 @@ var config = {
 firebase.initializeApp(config);
 
 var numberHaircuts = 0;
+var numberWorkouts = 0;
+var numberStudy = 0;
 
 function setup() {
     document.getElementById('haircutNo').innerHTML = numberHaircuts;
+    document.getElementById('workoutNo').innerHTML = numberWorkouts;
+    document.getElementById('studyNo').innerHTML = numberStudy;
 }
 
 function updateHaircuts(n) {
-    numberHaircuts+= n;
+    numberHaircuts += n;
     document.getElementById('haircutNo').innerHTML = numberHaircuts;
+}
+
+function updateWorkouts(n) {
+    numberWorkouts += n;
+    document.getElementById('workoutNo').innerHTML = numberWorkouts;
+}
+
+function updateStudy(n) {
+    numberStudy += n;
+    document.getElementById('studyNo').innerHTML = numberStudy;
 }
 
 function updateHaircuts2(n) {
